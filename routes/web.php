0c9d83dabff215001eb0f3a96532867abc37a712
Raffi Arditama <raffi20arditama@gmail.com>
2026-09-23 15:01:48 +0700

    Feature reservasi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\PetugasReservationController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes untuk pengguna yang telah terautentikasi
+Route::middleware('auth')->group(function () {
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::get('/my-reservations', [ReservationController::class, 'index']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+
+    // Routes khusus petugas
+    Route::prefix('petugas')->group(function () {
+        Route::get('/reservations', [PetugasReservationController::class, 'index']);
+        Route::put('/reservations/{id}/approve', [PetugasReservationController::class, 'approve']);
+        Route::put('/reservations/{id}/reject', [PetugasReservationController::class, 'reject']);
+        Route::put('/reservations/{id}/cancel', [PetugasReservationController::class, 'cancel']);
+    });
+});
+
